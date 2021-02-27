f0b7a0ce37da129f00b7530c6101ef3cf57e3ea3
MahabubArafat <arafatmahabub37@gmail.com>
2021-02-27 10:48:15 +0600

    heroku 1.1

--- server.py
+++ server.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app=Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+def write_to_csv(data):
+    with open("database.csv", newline="\n", mode="a") as database:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        csv_writter = csv.writer(
+            database, delimiter=",", quotechar="'", quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writter.writerow([name, email, message])
+
+@app.route("/submit_form",methods=["POST","GET"])
+def message():
+        if request.method=="POST":
+            data=request.form.to_dict()
+            name=data["name"]
+            write_to_csv(data)
+            return (f"thank you {name} for contacting us")
